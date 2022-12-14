b2f44f2ba3be1147bbf63ef3e33c0bd5bc658a2e
Nathom78 <99360304+Nathom78@users.noreply.github.com>
2022-12-14 18:52:32 +0100

    - Class Player_ID - suppression Class Players

--- models/players.py
+++ models/players.py
@@ -30,7 +30,6 @@ class Player(dict):
         self['sex'] = sex
         self['ranking'] = ranking
         self['score'] = score
-        self['score_last_match'] = 0
         self['Tournaments'] = []
 
     def __str__(self):
@@ -39,23 +38,46 @@ class Player(dict):
                f"- date of birth : {self['date_of_birth']}, " \
                f"sex : {self['sex']},\n" \
                f"- ranking : {self['ranking']}, " \
-               f"score : {self['score']},\n" \
-               f"score of the last match :{self['score_last_match']}.\n"
+               f"score : {self['score']}.\n"
 
     def modify(self):
         pass
+
+    @staticmethod
+    def correspond_player(player_temp: dict):
+        """ Test and put data from BD, in instance of Player"""
+        player = {}
+        try:
+            player = Player(player_temp['family_name'], player_temp['first_name'],
+                            player_temp['date_of_birth'], player_temp['sex'],
+                            player_temp['ranking'], player_temp['score'])
+        except(KeyError, TypeError,):
+            print(f"Base de donnée incorrect pour le joueur {player_temp}")
+        finally:
+            if not isinstance(player, Player):
+                raise ValueError("Joueur mal défini!")
+        return player
+
+    @staticmethod
+    def save(player):
+        db = TinyDB(PATH)
+        players_table = db.table(NAME_PLAYERS_TABLE)
+        if not isinstance(player, Player):
+            return ValueError("Joueur mal défini!")
+        players_table.insert(player)
 
 
 class PlayerId:
     """
     a player ID give a player dict, the players with ID is écolo
+    avec une list des ID des players d'un tournoi
 
     Pour l'instant je ne sais pas si j'aurais besoin d'instance de PlayerId sinon faire des
     @classmethod
     """
 
-    def __init__(self):  # à virer
-        self.players = Players()
+    def __init__(self):
+        self.players_IDs = []
 
     def print_list_player_sort_abc(self, list_players_id):  # à faire mieux et ménage
         list_players = self.ids_to_dicts(list_players_id)
@@ -71,6 +93,49 @@ class PlayerId:
         for x in range(len(players_sorted)):
             print(f"Joueur {x + 1}:\n{players_sorted[x]}")
 
+    def print_list_club(self, sort=0):
+        """ Retourne une liste prête à être affiché, de tous les joueurs
+        sort = 1 : rangé par ordre alphabétique
+        sort = 2 : rangé par score
+        sort = 0 : ordre de la base
+        """
+        if sort == 1:
+            self.print_list_player_sort_abc(self.players_IDs)
+        elif sort == 2:
+            self.print_list_player_sort_rank(self.players_IDs)
+        else:
+            list_player = self.ids_to_dicts(self.players_IDs)
+            for player in range(len(list_player)):
+                print(f"Joueur {player + 1}:\n{list_player[player]}")
+
+    def load_all(self):
+        # voir remplacer par une liste des IDs de la BD
+        db = TinyDB(PATH)
+        players_table = db.table(NAME_PLAYERS_TABLE)
+        list_temp = players_table.all()
+        self.players_IDs = []
+        for document in list_temp:
+            try:
+                Player.correspond_player(document)
+            except ValueError:
+                print(ValueError)
+            finally:
+                self.players_IDs.append(document.doc_id)
+
+    def ids_to_dicts(self, list_players_id):  # à faire mieux et ménage Players.list_player
+        """ Liste de joueur ID, retourne une liste de Player(dict)"""
+        list_player = []
+        player = {}
+        for player_id in list_players_id:
+            dict_player = self.id_to_dict(player_id)
+            try:
+                player = Player.correspond_player(dict_player)
+            except ValueError:
+                print(f"Base de donnée incorrect pour le joueur {player_id}")
+            finally:
+                list_player.append(player)
+        return list_player
+
     @staticmethod
     def id_to_dict(player_id):
         """ Joueur avec ID, retourne un document de la BD valide ou pas"""
@@ -78,91 +143,14 @@ class PlayerId:
         players_table = db.table('players')
         return players_table.get(doc_id=player_id)
 
-    def ids_to_dicts(self, list_players_id):  # à faire mieux et ménage Players.list_player
-        """ Liste de joueur ID, retourne une liste de Player(dict)"""
-        for player_id in list_players_id:
-            dict_player = self.id_to_dict(player_id)
-            try:
-                player_temp = Player(dict_player['family_name'], dict_player['first_name'],
-                                     dict_player['date_of_birth'], dict_player['sex'],
-                                     dict_player['ranking'], dict_player['score'])
-            except (KeyError, TypeError,):
-                print(f"Base de donnée incorrect pour le joueur {player_id}")
-            else:
-                self.players.append(player_temp)
-        return self.players
 
+# for i in range(8):
+#    Player.save(Player(f"Lebon{i + 1}", "Paul", "21/06/1969", "M", 15 + i))
 
-class Players(list):
-    """ Tout ce qui concernent tous les players de tous les tournois
-    - ajouter un player à la liste class.list_player (add)
-    - sauver dans la base de donnée tous les players class.save_all(nom du tournoi)
-    - afficher la liste class.list_player (print_list)
-    """
-
-    list_players: List[Player] = []  # voir List ID ou juste un Player
-
-    def __init__(self):
-        super().__init__()
-
-    def sort_abc(self):
-        pass
-
-    def append(self, player):
-        if not isinstance(player, Player):
-            return ValueError("Joueur mal défini!")
-        return super().append(player)
-
-    @classmethod
-    def add(cls, player):
-        if not isinstance(player, Player):
-            return ValueError("Joueur mal défini!")  # voir erreur ou faire try except
-        cls.list_players.append(player)
-
-    @classmethod
-    def save_all(cls):  # a effacer par save one
-        db = TinyDB(PATH)
-        players_table = db.table(NAME_PLAYERS_TABLE)
-        players_table.truncate()  # clear the table first
-        players_table.insert_multiple(cls.list_players)
-
-    @classmethod
-    def load_all(cls):  # voir remplacer par une liste des IDs de la BD
-        db = TinyDB(PATH)
-        players_table = db.table(NAME_PLAYERS_TABLE)
-        list_temp = players_table.all()
-        cls.list_players = []
-        for document in list_temp:
-            try:
-                cls.add(document)
-            except ValueError:
-                print(ValueError)
-
-    @classmethod
-    def print_list_club(cls):
-        """ Retourne une liste prête à être affiché, de tous les joueurs """
-        list_tom = ""
-        for player in range(len(cls.list_players)):
-            list_tom = list_tom + f"Joueur {player + 1}:\n{cls.list_players[player]}\n"
-        return list_tom
-
-    @classmethod
-    def modify_player(cls):
-        pass
-
-
-adidas = PlayerId()
-print(adidas.id_to_dict(2))
-tom = adidas.players
-print(tom)
-adidas.print_list_player_sort_rank([1, 2, 3, 4, 5, 6, 7, 8])
-
-
-"""
-tournament = Players
-for i in range(8):
-    tournament.list_players.append(Player(f"Lebon{i + 1}", "Paul", "21/06/1969", "M", 15))
-
-print(tournament.print_list_club())
-tournament.save_all('tournament')
-"""
+# adidas = PlayerId()
+# adidas.load_all()
+# tom = adidas.players_IDs
+# print(adidas.players_IDs)
+# adidas.print_list_club(0)
+# print(adidas.id_to_dict(3))
+# print((Player.correspond_player(Player("Lebon", "Paul", "21/06/1969", "M", 15))))
